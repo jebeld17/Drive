657bbb033cc68534b733c58147de690c470de8c1
jebeld17 <jebeld17@gmail.com>
2019-05-08 18:02:44 -0400

    Batch installer

--- batchinstall.bat
+++ batchinstall.bat
@@ -1,4 +1,4 @@
 @echo off
-PowerShell.exe -ExecutionPolicy Bypass -Command "& './#1.cmd'"
+PowerShell.exe -ExecutionPolicy Bypass -Command "& './#1.ps1'"
 PowerShell.exe -ExecutionPolicy Bypass -Command "& './#2.ps1'"
 PAUSE
